47ebdc305583905c376a4a26451f35df40b6bf8b
Vivek Kurhe <vivek.kurhe@ennoventure.com>
2023-03-26 20:46:49 +0530

    synching data across pages

--- chapter-07/notes-app10_editNotePagePart1_Part2/app.js
+++ chapter-07/notes-app10_editNotePagePart1_Part2/app.js
@@ -1,4 +1,4 @@
-const notes = getSavedNotes()
+let notes = getSavedNotes()
 
 const filters = {
     searchText: ''
@@ -25,4 +25,15 @@ document.querySelector('#search-text').addEventListener('input', (e) => {
 
 document.querySelector('#filter-by').addEventListener('change', (e) => {
     console.log(e.target.value)
+})
+
+// The storage event fires when any of the data in local storage changes
+// You can test by opening home page on one window and edit page on another window
+// Change in edit page will reflect on home page title dynamically without having to refresh it.
+
+window.addEventListener('storage', function (e) {
+    if(e.key === 'notes') {
+        notes = JSON.parse(e.newValue)
+        renderNotes(notes, filters)
+    }
 })
--- chapter-07/notes-app10_editNotePagePart1_Part2/notes-edit.js
+++ chapter-07/notes-app10_editNotePagePart1_Part2/notes-edit.js
@@ -2,8 +2,8 @@ const titleElement = document.querySelector('#note-title')
 const bodyElement = document.querySelector('#note-body')
 const removeElement = document.querySelector('#remove-note')
 const noteId = location.hash.substring(1)
-const notes = getSavedNotes()
-const note = notes.find(function(note) {
+let notes = getSavedNotes()
+let note = notes.find(function(note) {
     return note.id === noteId
 })
 
@@ -28,4 +28,23 @@ removeElement.addEventListener('click', function (e) {
     removeNote(note.id)
     saveNotes(notes)
     location.assign('/chapter-07/notes-app9_complexDOM/index.html')
+})
+
+// The storage event fires when any of the data in local storage changes
+// You can test by opening edit page on one tab and edit page on another tab
+// Change in edit page will reflect on another tab dynamically without having to refresh it.
+window.addEventListener('storage', function (e) {
+    if(e.key === 'notes') {
+        notes = JSON.parse(e.newValue)
+        note = notes.find(function (note) {
+            return note.id === noteId
+        })
+
+        if(note === undefined) {
+            location.assign('/index.html')
+        }
+
+        titleElement.value = note.title
+        bodyElement.value = note.body
+    }
 })
